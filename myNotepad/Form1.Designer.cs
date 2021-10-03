031d6cc802c0a330011c37a1490188b72b98036f
NikZapol <notbigsurpise@yandex.ru>
2021-10-03 22:40:00 +0300

    удлинил форму, покрасил в голубой

--- myNotepad/Form1.Designer.cs
+++ myNotepad/Form1.Designer.cs
@@ -67,8 +67,8 @@ namespace myNotepad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(152, 113);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(646, 113);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
